b0007baf7b715e144cc38be09012cfc0d012f618
kadir <akadirkkoc@gmail.com>
2020-03-14 16:46:07 +0300

    Ignore unmapped field

--- src/main/java/com/akadir/springdemo/mapper/ProductMapper.java
+++ src/main/java/com/akadir/springdemo/mapper/ProductMapper.java
@@ -3,6 +3,8 @@ package com.akadir.springdemo.mapper;
 import com.akadir.springdemo.dto.ProductDTO;
 import com.akadir.springdemo.entity.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
@@ -11,11 +13,12 @@ import java.util.List;
  * Date: 29.02.2020
  * Time: 17:03
  */
-@Mapper(componentModel = "spring")
+@Mapper(unmappedSourcePolicy = ReportingPolicy.IGNORE, componentModel = "spring")
 public interface ProductMapper {
     ProductDTO toProductDto(Product product);
 
     List<ProductDTO> toProductDTOList(List<Product> productList);
 
+    @Mapping(target = "createdAt", ignore = true)
     Product toProduct(ProductDTO productDTO);
 }
